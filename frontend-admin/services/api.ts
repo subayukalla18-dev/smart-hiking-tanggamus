121b681b77b99417a392a38270bfd6df43453588
imamalisaputra <imamalisaputra235@gmail.com>
2026-06-13 01:32:20 +0700

    update tanggal daftar akun pada user

--- frontend-admin/services/api.ts
+++ frontend-admin/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL:
-    "https://constantly-offerings-transparent-click.trycloudflare.com",
+    "https://promotes-reporting-eng-generous.trycloudflare.com",
 });
 
 // AUTO TOKEN
